6100cfd587f5384475bcf7e38a45c5c258c3f3e1
Jonas Ferreira (MAN Digital Hub) <jonas.ferreira-ext@man.eu>
2019-11-15 12:32:40 +0000

    feat: added script to deploy, added extra config for sms and script line to enable TOTP in CognitoUserPool

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 STACK_NAME="cloudformation-mfa-poc"
-aws s3 mb s3://$STACK_NAME --region us-west-1
+aws s3 mb s3://$STACK_NAME --region eu-central-1
 
 ## DELETE CLOUDFORMATION STACK
 echo 'Deleting the stack...'
@@ -11,3 +11,7 @@ echo 'Done'
 
 sam package --s3-bucket $STACK_NAME --s3-prefix $STACK_NAME --output-template-file packaged.yaml
 aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+aws cloudformation describe-stack-events --stack-name $STACK_NAME --query "StackEvents[?ResourceStatus == 'CREATE_FAILED'].ResourceStatusReason"
+
+user_pool_id=$(aws cognito-idp list-user-pools --max-results 20 --query 'UserPools[?Name==`Basys User Pool`].[Id]' --output text)
+aws cognito-idp set-user-pool-mfa-config --user-pool-id $user_pool_id   --mfa-configuration ON --software-token-mfa-configuration Enabled=true
